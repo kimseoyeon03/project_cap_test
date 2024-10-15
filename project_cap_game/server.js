15eea7973c258b77aea7633242bb10c1deecc2be
kimseoyeon03 <119295609+kimseoyeon03@users.noreply.github.com>
2024-10-15 17:59:55 +0900

    Update server.js

--- project_cap_game/server.js
+++ project_cap_game/server.js
@@ -5,7 +5,7 @@ const app = express();
 // public 폴더를 정적 파일 제공 경로로 설정
 app.use(express.static(path.join(__dirname, 'public')));
 
-// 3000번 포트에서 서버 실행
+
 app.listen(4000, () => {
   console.log('Server running at http://localhost:4000');
 });
